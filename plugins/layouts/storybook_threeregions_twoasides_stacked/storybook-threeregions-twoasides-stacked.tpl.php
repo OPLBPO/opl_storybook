2d3ab0ce8c3b18c654831688084421839394c9f5
Wendy Warren <wwarren@coldfrontlabs.ca>
2016-12-08 15:38:16 +0000

    feat(layout): Initial commit of custom module and custom layout - with the actual files

---
+++ plugins/layouts/storybook_threeregions_twoasides_stacked/storybook-threeregions-twoasides-stacked.tpl.php
@@ -0,0 +1,201 @@
+<div class="<?php print $classes ?>" <?php if (!empty($css_id)) { print "id=\"$css_id\""; } ?>>
+  <main id="oplfla-chapters">
+    
+    <?php if ($content['intro']): ?>
+      <section id="page-intro">
+        <div class="background-player">
+          <div class="hsContent" data-160-start="opacity: 1" data-750-start="opacity: 0">
+            <?php print $content['intro']; ?>
+            <header class="oplfla-header">
+              <hgroup>
+                <h1>OPL Food Literacy Main Heading</h1>
+                <h2>OPL Food Literacy Secondary Heading</h2>
+              </hgroup>
+            </header>
+            <div class="header-video">
+              <video autoplay loop>
+                <source src="<?php print base_path() . path_to_theme(); ?>/video/cooking-587.mp4" type="video/mp4">
+              </video>
+            </div>
+          </div>
+        </div>   
+      </section>
+    <?php endif ?>
+
+
+    <?php if ($content['region-1']): ?>
+
+      <?php print $content['region-1']; ?>
+
+      <section class="oplfla-article">
+        <div class="container">
+          <p><span class="dropcap">T</span>art sesame snaps bear claw carrot cake macaroon ice cream. Bonbon brownie cake croissant brownie. Tart brownie bonbon topping sugar plum liquorice tootsie roll. Danish marzipan marshmallow liquorice jelly powder pie jelly. Lemon drops powder oat cake. Marshmallow sweet apple pie gummi bears apple pie topping gummies sweet roll pie. Liquorice ice cream topping gingerbread. Brownie cake brownie donut. Fruitcake candy canes danish sweet. Cake pudding dessert sugar plum ice cream pie sweet candy cotton candy. Danish gingerbread liquorice cupcake. Donut icing jelly beans pastry cake sweet roll candy canes. Cotton candy chupa chups marzipan marshmallow caramels muffin. Cake cake liquorice chocolate cake chocolate cake wafer carrot cake chocolate cake biscuit.</p>
+          
+          <figure>
+            <img src="<?php print base_path() . path_to_theme(); ?>/images/storybook/winter-cooking4.jpg" alt="Winter cooking"/>
+            <figcaption>Lorem ipsum dolor sit amet, consectetur adipiscing elit, ut labore et dolore magna aliqua</figcaption>
+          </figure>
+          
+          <p>Bonbon marshmallow halvah marzipan. Chupa chups liquorice muffin halvah powder. Muffin toffee jelly-o macaroon muffin. Dragée marshmallow powder jujubes jujubes pie bear claw chocolate. Pie macaroon topping jelly lemon drops sweet roll. Cheesecake pudding chocolate cake. Cake pudding bonbon gingerbread. Bonbon tart biscuit pie marzipan marshmallow jelly-o jelly-o toffee. Jelly-o sesame snaps pie icing danish. Dessert tootsie roll bonbon fruitcake. Chupa chups jelly toffee jelly lemon drops lollipop dragée macaroon. Sugar plum muffin chocolate cake danish candy soufflé dragée sugar plum.</p>
+          <p>Gingerbread cupcake sugar plum marzipan gingerbread gingerbread jelly beans pie tootsie roll. Oat cake topping dragée jelly. Sweet oat cake icing marshmallow. Bear claw jujubes cupcake cookie tiramisu icing cotton candy. Croissant bonbon lollipop tootsie roll cotton candy. Halvah brownie tiramisu powder brownie candy pudding jelly beans. Chupa chups cookie bonbon caramels cake muffin chocolate bar gummies. Gingerbread bear claw fruitcake dessert croissant. Carrot cake cheesecake sweet chocolate bar croissant donut. Chupa chups soufflé topping gummies. Lollipop tart halvah cupcake wafer cheesecake. Gingerbread cotton candy sweet roll icing cupcake topping sugar plum. Danish gummies chocolate soufflé lollipop cheesecake.</p>
+          <p>Icing apple pie caramels chocolate cake soufflé jujubes tootsie roll danish. Oat cake chupa chups icing apple pie sesame snaps. Wafer cupcake pastry cheesecake gingerbread. Muffin sweet roll candy muffin gummi bears wafer cake. Ice cream macaroon macaroon gingerbread bear claw. Chupa chups bear claw tootsie roll sugar plum. Caramels sweet croissant brownie liquorice. Sesame snaps macaroon jujubes dragée toffee. Donut oat cake cotton candy cake fruitcake soufflé. Sugar plum marshmallow danish chupa chups oat cake jujubes apple pie soufflé. Toffee tart cake donut cheesecake cake croissant. Chupa chups apple pie soufflé danish powder jelly dessert donut. Lollipop cake cupcake sesame snaps topping.</p>
+        </div>
+      </section>
+
+    <?php endif ?>
+
+
+    <?php if ($content['aside-1']): ?>
+
+      <?php print $content['aside-1']; ?>
+
+      <aside class="recipe-list">
+        <div class="container flex">
+          <div>
+            <a class="recipe" href="http://www.google.com" data-100-bottom-top="transform: translateX(-200px); opacity: 0" data-center-top="transform: translateX(0px); opacity: 1">
+              <figure>
+                <img src="<?php print base_path() . path_to_theme(); ?>/images/storybook/winter-cooking1.jpeg" alt="Winter cooking"/>
+                <h3>Recipe: Chili</h3>
+              </figure>  
+            </a>
+            <a class="recipe" href="http://www.google.com" data-100-bottom-top="transform: translateY(200px); opacity: 0" data-center-top="transform: translateY(0px); opacity: 1">
+              <figure>  
+                <img src="<?php print base_path() . path_to_theme(); ?>/images/storybook/winter-cooking2.jpg" alt="Winter cooking"/>
+                <h3>Recipe: Chicken</h3>
+              </figure> 
+            </a>
+            <a class="recipe" href="http://www.google.com" data-100-bottom-top="transform: translateX(200px); opacity: 0" data-center-top="transform: translateX(0px); opacity: 1">
+              <figure> 
+                <img src="<?php print base_path() . path_to_theme(); ?>/images/storybook/winter-cooking3.png" alt="Winter cooking"/> 
+                <h3>Recipe: Pasta</h3>
+              </figure> 
+            </a>
+          </div>
+        </div>
+      </aside>    
+
+    <?php endif ?>
+
+
+    <?php if ($content['region-2']): ?>
+
+      <?php print $content['region-2']; ?>
+
+      <section class="oplfla-article">
+        <div class="container">
+          <p>Wafer donut sweet tootsie roll macaroon. Halvah cheesecake macaroon toffee chupa chups sweet roll. Toffee brownie jelly powder candy canes dessert dessert cake pie. Carrot cake chocolate bar marzipan. Bonbon oat cake jelly beans. Bear claw dessert cake lemon drops oat cake marshmallow apple pie cookie muffin. Wafer gummi bears jelly beans fruitcake jelly chupa chups cheesecake soufflé. Pie gingerbread lollipop sweet tart chocolate tiramisu candy chocolate. Cheesecake cupcake carrot cake oat cake tiramisu icing caramels. Cookie cotton candy sugar plum toffee. Caramels brownie pudding icing. Sugar plum marzipan cake carrot cake tiramisu. Powder gummi bears soufflé tootsie roll macaroon.</p>
+
+          <section class="region-podcast">
+              <article class="podcast-episode">
+                <h3>Podcast Title</h3>
+                <p>Cupcake ipsum dolor sit amet. Chupa chups ice cream fruitcake. Cheesecake chocolate danish bear claw pudding dragée cookie liquorice. </p>
+                <audio id="music" controls="controls">
+                  <source src="<?php print base_path() . path_to_theme(); ?>/podcast/20160329-130610.m4a" type="audio/mp4" />
+                </audio>
+              </article>
+              <article class="podcast-episode">
+                <h3>Podcast Title</h3>
+                <p>Apple pie brownie chocolate cake wafer jujubes icing cookie lemon drops. Lemon drops soufflé cake cotton candy bonbon.</p>
+                <audio id="music" controls="controls">
+                  <source src="<?php print base_path() . path_to_theme(); ?>/podcast/20160329-130610.m4a" type="audio/mp4" />
+                </audio>
+              </article>
+          </section>
+
+          <p>Topping lollipop gummies cotton candy cheesecake cheesecake sweet roll gummies soufflé. Liquorice cake gummi bears halvah apple pie. Donut soufflé gingerbread. Bear claw lemon drops sweet roll gummi bears chocolate cake pastry pie sugar plum pastry. Lemon drops tootsie roll ice cream pudding gummies tootsie roll. Oat cake lemon drops cheesecake sweet roll sweet cake brownie pastry fruitcake. Dragée cake halvah cotton candy gummies toffee wafer tiramisu bonbon. Dragée brownie toffee macaroon biscuit. Sweet candy canes jelly sweet roll jelly sugar plum. Halvah icing candy oat cake. Cotton candy halvah powder sugar plum cotton candy tart. Chocolate cake caramels gingerbread gingerbread pie dragée pudding chocolate cake.</p>
+          <p>Fruitcake tiramisu gummi bears powder. Soufflé icing sweet roll gummi bears sweet roll cake croissant marzipan. Apple pie marzipan dessert oat cake. Jujubes cheesecake tootsie roll fruitcake. Tart tootsie roll pastry apple pie sweet bear claw pudding. Tiramisu gingerbread lollipop jujubes chocolate cake wafer ice cream pie gingerbread. Candy canes jelly beans gummi bears. Soufflé sweet candy canes tart muffin cotton candy caramels. Gummi bears lemon drops lollipop liquorice jujubes jujubes cupcake lemon drops. Powder apple pie dragée dragée. Sesame snaps chocolate bar sugar plum candy powder jelly icing. Danish topping croissant. Macaroon dragée cupcake dessert jelly-o.</p>
+        </div>
+      </section>      
+
+    <?php endif ?>
+
+
+    <?php if ($content['aside-2']): ?>
+
+      <?php print $content['aside-2']; ?>
+
+      <aside class="collection-list">
+        <div class="container flex">
+          <div>
+            <a class="collection-item" href="http://www.google.com">
+              <figure>
+                <img src="<?php print base_path() . path_to_theme(); ?>/images/storybook/slow-cooking1.jpg" alt="Winter cooking"/>
+                <figcaption></figcaption>
+              </figure>
+            </a>
+            <a class="collection-item" href="http://www.google.com">
+              <figure>  
+                <img src="<?php print base_path() . path_to_theme(); ?>/images/storybook/slow-cooking2.jpg" alt="Winter cooking"/>
+                <figcaption></figcaption>
+              </figure>
+            </a>
+            <a class="collection-item" href="http://www.google.com">
+              <figure> 
+                <img src="<?php print base_path() . path_to_theme(); ?>/images/storybook/slow-cooking3.jpg" alt="Winter cooking"/>
+                <figcaption></figcaption>
+              </figure>
+            </a>
+            <a class="collection-item" href="http://www.google.com">
+              <figure>  
+                <img src="<?php print base_path() . path_to_theme(); ?>/images/storybook/slow-cooking4.jpg" alt="Winter cooking"/>
+                <figcaption></figcaption>
+              </figure>
+            </a>
+            <a class="collection-item" href="http://www.google.com">
+              <figure> 
+                <img src="<?php print base_path() . path_to_theme(); ?>/images/storybook/slow-cooking5.jpg" alt="Winter cooking"/>
+                <figcaption></figcaption>
+              </figure>
+            </a>
+            <a class="collection-item" href="http://www.google.com">
+              <figure>
+                <img src="<?php print base_path() . path_to_theme(); ?>/images/storybook/slow-cooking1.jpg" alt="Winter cooking"/>
+                <figcaption></figcaption>
+              </figure>
+            </a>
+          </div>
+        </div>
+      </aside>      
+
+    <?php endif ?>
+
+
+    <?php if ($content['region-3']): ?>
+
+      <?php print $content['region-3']; ?>
+
+      <section id="article-part3" class="oplfla-article">
+        <div class="container">
+          <p>Cookie croissant cotton candy carrot cake cake sesame snaps chocolate pastry. Caramels wafer icing. Ice cream lollipop lemon drops danish croissant. Tart jelly beans toffee sugar plum gummi bears muffin macaroon. Gummi bears chocolate cake ice cream dragée cupcake macaroon pudding jelly beans marshmallow. Sweet dragée biscuit. Cupcake marzipan dessert oat cake. Marzipan cake macaroon biscuit tootsie roll. Wafer cookie soufflé pudding gingerbread. Brownie cheesecake pudding sugar plum. Pudding sweet gummies jelly icing. Sesame snaps croissant jelly.</p>
+
+          <section class="region-youtube">
+            <div class='embed-container'>
+              <iframe width="560" height="315" src='http://www.youtube.com/embed/c1_V2y_9KCM' frameborder='0' allowfullscreen></iframe>
+            </div>
+          </section> 
+
+          <p>Toffee cake chocolate cake sesame snaps. Dessert macaroon cupcake toffee cheesecake chocolate cookie. Tootsie roll topping carrot cake apple pie pastry brownie gummies. Jelly gummies sesame snaps gummi bears bear claw halvah. Brownie cookie powder. Dessert oat cake pudding chocolate cake marzipan pastry candy cake. Liquorice pastry sugar plum soufflé cake biscuit sweet chupa chups. Chocolate bar chocolate bar sugar plum donut pastry. Pudding biscuit bear claw. Cake cake cookie muffin croissant dragée cookie bear claw. Gummi bears fruitcake pudding. Marshmallow cake lemon drops soufflé donut sugar plum macaroon macaroon jujubes.</p>
+          <p>Tiramisu sweet cookie cotton candy. Bonbon liquorice bear claw lemon drops chocolate caramels tootsie roll jelly-o. Lemon drops apple pie gingerbread muffin gummies icing. Apple pie cotton candy icing chocolate bar. Cotton candy gummies lemon drops chupa chups chocolate marzipan oat cake cupcake donut. Cake dessert muffin bear claw gummies. Fruitcake macaroon dessert tootsie roll marzipan biscuit. Halvah macaroon biscuit soufflé cotton candy carrot cake cookie. Bonbon sweet bear claw wafer. Dragée chupa chups danish chupa chups sesame snaps gingerbread jelly. Macaroon sesame snaps chupa chups topping jelly-o cotton candy chocolate bar. Sugar plum danish croissant apple pie. Chocolate tiramisu macaroon. Cotton candy danish sugar plum chupa chups donut halvah jelly-o gummi bears cupcake.</p>
+        </div>
+      </section>      
+
+    <?php endif ?>
+
+
+    <?php if ($content['footer']): ?>
+
+      <?php print $content['footer']; ?>
+
+      <footer id="oplfla-footer">
+        <div class="full-bg-img" data-600-end="opacity: 1;" data-end="opacity: 0.7;">
+        </div>
+        <div class="container credits" data-600-end="opacity: 0.7;" data-end="opacity: 1;">
+            <h3>Credits</h3>
+            <p>Cookie croissant cotton candy carrot cake cake sesame snaps chocolate pastry. Caramels wafer icing. Ice cream lollipop lemon drops danish croissant. Tart jelly beans toffee sugar plum gummi bears muffin macaroon. Gummi bears chocolate cake ice cream dragée cupcake macaroon pudding jelly beans marshmallow. Sweet dragée biscuit. Cupcake marzipan dessert oat cake. Marzipan cake macaroon biscuit tootsie roll. Wafer cookie soufflé pudding gingerbread. Brownie cheesecake pudding sugar plum. Pudding sweet gummies jelly icing. Sesame snaps croissant jelly.</p>
+        </div>
+      </footer>
+
+    <?php endif ?>
+
+  </main>
+</div>
